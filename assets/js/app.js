a8813821932709e4f78d34a8dd1b54c112d0bbab
Emily Amspoker <eamspoker21@kentdenver.org>
2021-05-05 21:19:30 -0600

    init commit -- does not work on mobile

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+var constraints = { video: { facingMode: "environment" }, audio: false };
+const cameraView = document.querySelector("#camera--view"),
+      cameraOutput = document.querySelector("#camera--output"),
+      cameraSensor = document.querySelector("#camera--sensor"),
+      cameraTrigger = document.querySelector("#camera--trigger")
+
+function cameraStart() {
+    var err = document.getElementById("error");
+    err.innerText = getUserMediaSupported();
+  navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function(stream) {
+      track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
+  })
+  .catch(function(error) {
+      console.error("Oops. Something is broken.", error);
+      
+  });
+}
+
+cameraTrigger.onclick = function() {
+};
+
+
+function getUserMediaSupported() {
+    return !!(navigator.mediaDevices &&
+      navigator.mediaDevices.getUserMedia);
+  }
+
+window.addEventListener("load", cameraStart, false);
