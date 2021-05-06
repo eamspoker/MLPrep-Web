90cc41e52169568d44afac858ffd334ebed64736
Emily Amspoker <eamspoker21@kentdenver.org>
2021-05-06 12:18:59 -0600

    highlight and label predictions

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ const cameraView = document.querySelector("#camera--view"),
       cameraTrigger = document.querySelector("#camera--trigger"),
       camera = document.getElementById("camera"),
       loading = document.getElementById("loading"),
+      liveView = document.getElementById("liveView"),
       content = document.getElementById("content");
 var isOn = false;
 var localstream;
@@ -50,43 +51,44 @@ var children = [];
 
 function predictWebcam() {
   // Now let's start classifying a frame in the stream.
-  model.detect(cameraView).then(function (predictions) {
-    //Remove any highlighting we did previous frame.
-    for (let i = 0; i < children.length; i++) {
-      camera.removeChild(children[i]);
+model.detect(cameraView).then(function (predictions) {
+  // Remove any highlighting we did previous frame.
+  for (let i = 0; i < children.length; i++) {
+    liveView.removeChild(children[i]);
+  }
+  children.splice(0);
+
+  // Now lets loop through predictions and draw them to the live view if
+  // they have a high confidence score.
+  var food = ["banana", "apple", "orange", "carrot", "broccoli", "hot dog"]
+  for (let n = 0; n < predictions.length; n++) {
+    // If we are over 66% sure we are sure we classified it right, draw it!
+    if (food.includes(predictions[n].class) && predictions[n].score > 0.66) {
+      const p = document.createElement('p');
+      p.innerText = predictions[n].class
+      p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
+          + (predictions[n].bbox[1] - 10) + 'px; width: '
+          + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+
+      const highlighter = document.createElement('div');
+      highlighter.setAttribute('class', 'highlighter');
+      highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+          + predictions[n].bbox[1] + 'px; width: '
+          + predictions[n].bbox[2] + 'px; height: '
+          + predictions[n].bbox[3] + 'px;';
+
+      console.log(highlighter.innerText);
+
+      liveView.appendChild(highlighter);
+      liveView.appendChild(p);
+      children.push(highlighter);
+      children.push(p);
     }
-    children.splice(0);
+  }
 
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
-    for (let n = 0; n < predictions.length; n++) {
-      // If we are over 66% sure we are sure we classified it right, draw it!
-      if (predictions[n].score > 0.66) {
-        const p = document.createElement('p');
-        p.innerText = predictions[n].class  + ' - with '
-            + Math.round(parseFloat(predictions[n].score) * 100)
-            + '% confidence.';
-        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-            + (predictions[n].bbox[1] - 10) + 'px; width: '
-            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
-
-        const highlighter = document.createElement('div');
-        highlighter.setAttribute('class', 'highlighter');
-        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-            + predictions[n].bbox[1] + 'px; width: '
-            + predictions[n].bbox[2] + 'px; height: '
-            + predictions[n].bbox[3] + 'px;';
-
-        camera.appendChild(highlighter);
-        camera.appendChild(p);
-        children.push(highlighter);
-        children.push(p);
-      }
-    }
-
-    // Call this function again to keep predicting when the browser is ready.
-    window.requestAnimationFrame(predictWebcam);
-  });
+  // Call this function again to keep predicting when the browser is ready.
+  window.requestAnimationFrame(predictWebcam);
+});
 }
 
 var model = undefined;
