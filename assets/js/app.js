89111f8943f61b2208407a7d681c578749fa4716
Emily Amspoker <eamspoker21@kentdenver.org>
2021-05-05 21:48:59 -0600

    add enable button

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,29 +3,45 @@ const cameraView = document.querySelector("#camera--view"),
       cameraOutput = document.querySelector("#camera--output"),
       cameraSensor = document.querySelector("#camera--sensor"),
       cameraTrigger = document.querySelector("#camera--trigger")
+const isOn = false;
+
+if (getUserMediaSupported()) {
+  cameraTrigger.addEventListener('click', cameraStart);
+} else {
+  console.warn('getUserMedia() is not supported by your browser');
+}
 
 function cameraStart() {
-    var err = document.getElementById("error");
-    err.innerText = getUserMediaSupported();
-  navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(function(stream) {
-      track = stream.getTracks()[0];
-      cameraView.srcObject = stream;
-  })
-  .catch(function(error) {
-      console.error("Oops. Something is broken.", error);
-      
+  // Only continue if the COCO-SSD has finished loading.
+  if (!model) {
+    return;
+  }
+
+  if(isOn){
+    return;
+  }
+
+  // Hide the button once clicked.
+  event.target.classList.add('removed');
+
+  // getUsermedia parameters to force video but not audio.
+  const constraints = {
+    video: true
+  };
+
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadeddata', predictWebcam);
   });
 }
 
-cameraTrigger.onclick = function() {
-};
+function predictWebcam() {
+}
 
+var model = true;
 
 function getUserMediaSupported() {
     return !!(navigator.mediaDevices &&
       navigator.mediaDevices.getUserMedia);
   }
-
-window.addEventListener("load", cameraStart, false);
